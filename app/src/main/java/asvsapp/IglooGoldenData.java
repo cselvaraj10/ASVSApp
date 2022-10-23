86c711f1bf71b40a0ede68f802e815a584bdb472
cselvaraj10 <chitraselvaraj@iheartmedia.com>
2022-10-23 19:41:39 -0400

    parseddata

--- app/src/main/java/asvsapp/IglooGoldenData.java
+++ app/src/main/java/asvsapp/IglooGoldenData.java
@@ -1,36 +1,10 @@
 package asvsapp;
 
-import java.util.List;
-
 public class IglooGoldenData {
- 
-    int rank, total_occerences; 
-    String search_term;
-    private List<?>  ssr, qsr;
-
-    public int getRank() {
-        return rank;
-    }
-
-    public int getTotalOccurences(){
-        return total_occerences;
-    }
-
-    public String getSearchTerm(){
-        return search_term;
-    }
- 
-    public List<?>  getSSR() {
-        return ssr;
-    }
-
-    public List<?> getQSR() {
-        return qsr;
-    }
- 
-}
-
-
-
-
+    private float rank;
+    private float total_occurrences;
+    private String search_term;
+    ssr ssr;
+    qsr qsr;
     
+}
---
+++ app/src/main/java/asvsapp/ssr.java
@@ -0,0 +1,8 @@
+package asvsapp;
+
+public class ssr {
+    private String content_type;
+    private String content_id;
+    private String content_name;
+    private int occurrences;
+}
